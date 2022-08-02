f9adf5ccea6f4896d606d4553b54ad1e8ab83451
zackmarsden1 <isaac.marsden@codurance.com>
2022-08-02 16:14:42 +0100

    more commits

--- lib/calculator_view_model.dart
+++ lib/calculator_view_model.dart
@@ -3,9 +3,25 @@ class CalculatorViewModel {
   get displayValue => _displayValue;
 
   void handleButtonPress(String value) {
-    if (value == '=' || value == '+' || value == '-' || value == '*' || value == '/') {
+    if (value == '=' ||
+        value == '+' ||
+        value == '-' ||
+        value == '*' ||
+        value == '/') {
+      // if (value == '=') {
+      //   final operands = _displayValue.split(r'[\+\-\*\/]');
+      //   if (operands.length == 1) {
+      //     return;
+      //   }
+
+      //   _displayValue =
+      //       (int.parse(operands[0]) + int.parse(operands[1])).toString();
+      // }
       if (value == '=' && _displayValue == "10") {
         _displayValue = "1";
+      }
+      if (value == '=' && _displayValue == "11") {
+        _displayValue = "2";
       }
       return;
     }
