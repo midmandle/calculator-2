3ea75eb50c08fd290cd5a8e212aea57d08906159
Toby <tobywoollaston@hotmail.co.uk>
2022-08-03 10:32:58 +0100

    updated calculator to use new model

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,5 +1,6 @@
 import 'package:calculator_2/number_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'calculator_view_model.dart';
 
@@ -13,95 +14,99 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
+    return Consumer<CalculatorViewModel>(
+      builder: (context, viewModel, _) {
+        return Column(
           children: [
-            Text(widget.viewModel.displayValue,
-                textDirection: TextDirection.ltr)
-          ],
-        ),
-        Row(
-          children: [
-            NumberButton(
-              value: '/',
-              onClickHandler: widget.viewModel.handleButtonPress,
+            Row(
+              children: [
+                Text(viewModel.displayValue,
+                    textDirection: TextDirection.ltr)
+              ],
+            ),
+            Row(
+              children: [
+                NumberButton(
+                  value: '/',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                NumberButton(
+                  value: '7',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '8',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '9',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '*',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                NumberButton(
+                  value: '4',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '5',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '6',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '-',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                NumberButton(
+                  value: '1',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '2',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '3',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '+',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                NumberButton(
+                  value: '0',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+                NumberButton(
+                  value: '=',
+                  onClickHandler: viewModel.handleButtonPress,
+                ),
+              ],
             ),
           ],
-        ),
-        Row(
-          children: [
-            NumberButton(
-              value: '7',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '8',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '9',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '*',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            NumberButton(
-              value: '4',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '5',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '6',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '-',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            NumberButton(
-              value: '1',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '2',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '3',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '+',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            NumberButton(
-              value: '0',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-            NumberButton(
-              value: '=',
-              onClickHandler: widget.viewModel.handleButtonPress,
-            ),
-          ],
-        ),
-      ],
+        );
+      },
     );
   }
 }
